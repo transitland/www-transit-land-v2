0aeb46795bc60392f16be3a07f6dd7cf20ed7bb7
Ian Rees <ian@ianrees.net>
2019-11-15 12:19:57 -0800

    Remove unused files

--- gotransit-ui/nuxt.config.js
+++ gotransit-ui/nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.2.0/dist/leaflet.css' },
+      { rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.2.0/dist/leaflet.css' }
     ]
   },
   /*
@@ -35,7 +35,7 @@ export default {
   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    // '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module'
   ],
   /*
   ** Nuxt.js modules
@@ -59,11 +59,11 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
+    extend (config, ctx) {
       config.module.rules.push({
         test: /\.(graphql|gql)$/,
         exclude: /node_modules/,
-        loader: 'graphql-tag/loader',
+        loader: 'graphql-tag/loader'
       })
     }
   }
