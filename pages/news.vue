067d3d2d90ba460c5fa9338947b803936c2a5e0b
Ian Rees <ian@ianrees.net>
2020-07-21 12:01:52 -0700

    Cache invalidation step

--- pages/news.vue
+++ pages/news.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>News</div>
+  <div>
+    <h1 class="title is-2">
+      News
+    </h1>
+    <a class="twitter-timeline" href="https://twitter.com/transitland?ref_src=twsrc%5Etfw">Latest updates</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
+  </div>
 </template>
