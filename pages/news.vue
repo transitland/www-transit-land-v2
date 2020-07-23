bbfbc634c673ccb1fa1b1b0392586772c6121aaa
Drew Dara-Abrams <drew@interline.io>
2020-07-23 10:38:56 -0700

    text & nav improvements

--- pages/news.vue
+++ pages/news.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <h1 class="title is-2">
-      News
+      News & Updates
     </h1>
-    <a class="twitter-timeline" href="https://twitter.com/transitland?ref_src=twsrc%5Etfw">Latest updates</a>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
+    <p class="content is-medium">
+      For updates on Transitland, see <a href="https://transit.land/news/">See the blog on Transitland v1</a> and <a href="https://twitter.com/transitland">@transitland on Twitter</a>.
+    </p>
+    <hr>
+    <a class="twitter-timeline" data-dnt="true" data-theme="dark" href="https://twitter.com/transitland?ref_src=twsrc%5Etfw">Tweets by transitland</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
   </div>
 </template>
+
+<script>
+export default {
+  head: {
+    title: 'News & Updates'
+  }
+}
+</script>
