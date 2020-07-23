2e956fb67cdea1d4499dd721bb6fe8217f0ed99d
Ian Rees <ian@ianrees.net>
2020-07-22 22:58:49 -0700

    WIP

--- pages/operators/_operator/index.vue
+++ pages/operators/_operator/index.vue
@@ -13,26 +13,31 @@
           {{ $route.params.operator }}
         </nuxt-link>
       </h1>
+
+      <b-table
+        :data="associatedFeeds"
+        :striped="true"
+        sort-icon="menu-up"
+      >
+        <template slot-scope="props">
+          <b-table-column label="Feed" field="feed_onestop_id">
+            {{ props.row.feed_onestop_id }}
+          </b-table-column>
+          <b-table-column label="Agency" field="gtfs_agency_id">
+            {{ props.row.gtfs_agency_id }}
+          </b-table-column>
+          <b-table-column label="Matched">
+            {{ findAgencyMatch(props.row) }}
+          </b-table-column>
+        </template>
+      </b-table>
+
+      <br>
+
       <b-tabs v-model="activeTab" type="is-boxed">
         <b-tab-item label="Map">
           <!-- need fvids for good index search -->
           <feed-version-map-viewer :fvids="fvids" :agency-ids="agencyIds" :overlay="true" />
-        </b-tab-item>
-        <b-tab-item label="Data sources">
-          <b-table
-            :data="agencies"
-            :striped="true"
-            sort-icon="menu-up"
-          >
-            <template slot-scope="props">
-              <b-table-column field="agency" label="Source feed">
-                {{ props.row.feed_version.current_feed.onestop_id }}
-              </b-table-column>
-              <b-table-column field="agency" label="Source agency">
-                {{ props.row.agency_name }}
-              </b-table-column>
-            </template>
-          </b-table>
         </b-tab-item>
         <b-tab-item label="Routes">
           <route-viewer v-if="activeTab === 2" :agency-ids="agencyIds" :fvids="fvids" :show-agency="true" />
@@ -71,6 +76,9 @@ export default {
     }
   },
   computed: {
+    entity () {
+      return this.entities.length > 0 ? this.entities[0] : null
+    },
     agencies () {
       const ret = []
       for (const ent of this.entities) {
@@ -80,11 +88,29 @@ export default {
       }
       return ret
     },
+    associatedFeeds () {
+      const ret = []
+      for (const a of this.entity.associated_feeds || []) {
+        ret.push(a)
+      }
+      return ret
+    },
     agencyIds () {
       return this.agencies.map((s) => { return s.id }).filter((s) => { return s })
     },
     fvids () {
       return this.agencies.map((s) => { return s.feed_version.id })
+    }
+  },
+  methods: {
+    findAgencyMatch (assoc) {
+      for (const a of this.agencies) {
+        console.log(a, assoc)
+        if (a.feed_version.current_feed.onestop_id === assoc.feed_onestop_id && a.agency_id === assoc.gtfs_agency_id) {
+          return 'Yes'
+        }
+      }
+      return null
     }
   },
   head () {
