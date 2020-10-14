5c51b42dbd55888a90f8b94a1a60a043c67f74eb
Drew Dara-Abrams <drew@interline.io>
2020-10-14 15:01:00 -0700

    docs menu

--- components/doc-index.vue
+++ components/doc-index.vue
@@ -1,24 +1,33 @@
 <template>
   <div>
     <b-menu>
-      <b-menu-list v-for="item of nav" :key="item.title" expanded>
-        <template slot="label">
-          <nuxt-link v-if="item.link" :to="item.link">
-            {{ item.title }}
-          </nuxt-link>
-          <template v-else>
-            {{ item.title }}
-          </template>
-        </template>
-        <b-menu-item v-for="child of item.items || []" :key="child.title">
+      <b-menu-list label="Documentation" expanded>
+        <b-menu-item
+          v-for="item of nav"
+          :key="item.title"
+          expanded
+        >
           <template slot="label">
-            <nuxt-link v-if="child.link" :to="child.link">
-              {{ child.title }}
+            <nuxt-link v-if="item.link" :to="item.link" exact>
+              {{ item.title }}
             </nuxt-link>
             <template v-else>
-              {{ child.title }}
+              {{ item.title }}
             </template>
           </template>
+          <b-menu-item
+            v-for="child of item.items || []"
+            :key="child.title"
+          >
+            <template slot="label">
+              <nuxt-link v-if="child.link" :to="child.link">
+                {{ child.title }}
+              </nuxt-link>
+              <template v-else>
+                {{ child.title }}
+              </template>
+            </template>
+          </b-menu-item>
         </b-menu-item>
       </b-menu-list>
     </b-menu>
@@ -34,35 +43,50 @@ export default {
           title: 'Overview',
           link: '/documentation/',
           items: [
-            { title: 'Architecture', link: '/documentation/index#transitland-architecture' },
-            { title: 'Transitland APIs', link: '/documentation/index#comparing-the-apis' }
+            {
+              title: 'Technical Architecture',
+              link: '/documentation/index#transitland-architecture'
+            },
+            {
+              title: 'Comparing the APIs',
+              link: '/documentation/index#comparing-the-apis'
+            }
           ]
         },
         { title: 'Transitland Atlas', link: '/documentation/atlas' },
-        { title: 'Onestop ID Scheme', link: '/documentation/onestop-id-scheme/' },
         {
-          title: 'Transitland v2 API',
-          items: [
-            { title: 'Vector Tiles', link: '/documentation/vector-tiles' },
-            { title: 'Rest API', link: '/documentation/rest-api' },
-            { title: 'GraphQL API', link: '/documentation/graphql-api' }
-          ]
+          title: 'Onestop ID Scheme',
+          link: '/documentation/onestop-id-scheme/'
         },
         {
-          title: 'Transitland v1 API', link: '/documentation/v1'
+          title: 'Transitland v2 Vector Tiles API',
+          link: '/documentation/vector-tiles'
+        },
+        {
+          title: 'Transitland v2 REST API',
+          link: '/documentation/rest-api'
+        },
+        {
+          title: 'Transitland v2 GraphQL API',
+          link: '/documentation/graphql-api'
+        },
+        {
+          title: 'Transitland v1 Datastore API',
+          link: '/documentation/v1'
         },
         {
           title: 'Legal',
           items: [
             {
-              title: 'An Open Project', link: '/documentation/an-open-project/index'
+              title: 'An Open Project',
+              link: '/documentation/an-open-project/index'
             },
             {
-              title: 'Contributor Agreement', link: '/documentation/an-open-project/contributor-agreement'
+              title: 'Contributor Agreement',
+              link: '/documentation/an-open-project/contributor-agreement'
             }
           ]
         }
-
       ]
     }
   }
