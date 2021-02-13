328ec565a208403919a17083f8c37461c12fd479
Ian Rees <ian@ianrees.net>
2021-02-12 18:59:36 -0800

    Re-enable console for testing

---
+++ pages/documentation/api/graphql-console.vue
@@ -0,0 +1,16 @@
+<template>
+  <iframe src="/graphql-console.html" />
+</template>
+
+<script>
+export default {
+  layout: 'map'
+}
+</script>
+
+<style lang="scss" scoped>
+  iframe {
+    width: 100%;
+    height: calc(100vh - 60px);
+  }
+</style>
