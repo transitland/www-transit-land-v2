3c15cdd0b4f6065499e44184a1c9aa31cde5f7de
Drew Dara-Abrams <drew@interline.io>
2021-02-26 20:26:13 -0800

    simple analytics should only run on client, not ssr

--- components/simple-analytics.vue
+++ components/simple-analytics.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <script async defer src="https://sa.transit.land/app.js" />
-    <noscript inline-template><img src="https://sa.transit.land/image.gif" alt="" /></noscript>
+    <client-only>
+      <script async defer src="https://sa.transit.land/app.js" />
+      <noscript><img src="https://sa.transit.land/image.gif" alt="" /></noscript>
+    </client-only>
   </div>
 </template>
